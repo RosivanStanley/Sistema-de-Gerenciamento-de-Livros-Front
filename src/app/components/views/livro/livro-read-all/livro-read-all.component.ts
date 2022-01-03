250acba24265b8ffa4224e226a710836f5c293d2
RosivanStanley <rosivanstanley@hotmail.com>
2022-01-03 16:48:24 -0300

    Implementando LIVRO-CREATE

--- src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -15,7 +15,11 @@ export class LivroReadAllComponent implements OnInit {
 
   livros: Livro[] = [];
 
-  constructor(private service: LivroService, private route: ActivatedRoute) {}
+  constructor(
+    private service: LivroService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
@@ -27,5 +31,9 @@ export class LivroReadAllComponent implements OnInit {
       this.livros = resposta;
       console.log(this.livros);
     });
+  }
+
+  navegarParaCriarLivro(): void {
+    this.router.navigate([`categorias/${this.id_cat}/livros/create`]);
   }
 }
